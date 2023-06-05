b072d65268cb299d3dd52f8253e1bccd222a1b39
nyanshiba1 <nyanshiba77@gmail.com>
2023-06-05 14:30:58 +0900

    graphql_commit

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-
-import Helmet from "helmet";
-
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // ここにセキュリティについての設定を追加する
-  app.use(Helmet());
-
-  // NestJS はこれでCORS の設定を追加することができる
-  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
